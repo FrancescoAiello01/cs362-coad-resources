0671570a65f8ee40b009103e5d2c7b03e5c12e9e
Carson-Key <keyc@oregonstate.edu>
2020-04-30 14:34:27 -0700

    Add attribute tests

--- spec/models/ticket_spec.rb
+++ spec/models/ticket_spec.rb
@@ -6,7 +6,16 @@ RSpec.describe Ticket, type: :model do
 
   describe "attributes" do
 
-
+    specify { expect(ticket).to respond_to(:name) }
+    specify { expect(ticket).to respond_to(:description) }
+    specify { expect(ticket).to respond_to(:phone) }
+    specify { expect(ticket).to respond_to(:organization_id) }
+    specify { expect(ticket).to respond_to(:created_at) }
+    specify { expect(ticket).to respond_to(:updated_at) }
+    specify { expect(ticket).to respond_to(:closed) }
+    specify { expect(ticket).to respond_to(:closed_at) }
+    specify { expect(ticket).to respond_to(:resource_category_id) }
+    specify { expect(ticket).to respond_to(:region_id) }
 
   end
 
