9bf39cb3690c17b43cdf1efc4ba18216dfdd804d
Carson-Key <keyc@oregonstate.edu>
2020-04-30 14:29:22 -0700

    Outline tests for tickets

--- spec/models/ticket_spec.rb
+++ spec/models/ticket_spec.rb
@@ -2,4 +2,24 @@ require 'rails_helper'
 
 RSpec.describe Ticket, type: :model do
 
+  let(:ticket) { Ticket.new() }
+
+  describe "attributes" do
+
+
+
+  end
+
+  describe "relationships" do
+
+
+
+  end
+
+  describe "validations" do
+
+
+
+  end
+
 end
