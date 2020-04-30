cca55780410386ac0319b5cd2587aad50e949cfb
Carson-Key <keyc@oregonstate.edu>
2020-04-30 15:25:27 -0700

    Outline resource_category tests

--- spec/models/resource_category_spec.rb
+++ spec/models/resource_category_spec.rb
@@ -2,4 +2,24 @@ require 'rails_helper'
 
 RSpec.describe ResourceCategory, type: :model do
 
+  let(:resource_category) { ResourceCategory.new() }
+
+  describe "attributes" do
+
+
+
+  end
+
+  describe "relationships" do
+
+
+
+  end
+
+  describe "validations" do
+
+
+
+  end
+
 end
