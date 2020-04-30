59fc7118b2730f99af4b3847485d094921cfeec4
Carson-Key <keyc@oregonstate.edu>
2020-04-30 14:36:25 -0700

    Add relationship tests

--- spec/models/ticket_spec.rb
+++ spec/models/ticket_spec.rb
@@ -21,7 +21,17 @@ RSpec.describe Ticket, type: :model do
 
   describe "relationships" do
 
+    it "belongs to region" do
+        expect(ticket).to belong_to(:region)
+    end
 
+    it "belongs to resource_category" do
+        expect(ticket).to belong_to(:resource_category)
+    end
+
+    it "belongs to organization" do
+        expect(ticket).to belong_to(:organization).optional
+    end
 
   end
 
