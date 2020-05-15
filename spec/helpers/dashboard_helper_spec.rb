513724617ee11d8725bdb8fd2b1f740136814ea5
Francesco Aiello <aiellof@oregonstate.edu>
2020-05-15 09:46:42 -0700

    adds dashboard helper spec

--- spec/helpers/dashboard_helper_spec.rb
+++ spec/helpers/dashboard_helper_spec.rb
@@ -12,4 +12,33 @@ require 'rails_helper'
 # end
 RSpec.describe DashboardHelper, type: :helper do
 
+  it "returns admin dashboard" do
+    user = double()
+    user.stub(:admin?).and_return(true)
+    expect(dashboard_for(user)).to eq('admin_dashboard')
+  end
+
+  it "returns organization_submitted_dashboard" do
+    User.any_instance.stub(:admin?).and_return(false)
+    user = User.new
+    user.stub_chain(:organization, :submitted?).and_return(true)
+    expect(dashboard_for(user)).to eq('organization_submitted_dashboard')
+  end
+
+  it "returns organization_approved_dashboard" do
+    User.any_instance.stub(:admin?).and_return(false)
+    user = User.new
+    user.stub_chain(:organization, :submitted?).and_return(false)
+    user.stub_chain(:organization, :approved?).and_return(true)
+    expect(dashboard_for(user)).to eq('organization_approved_dashboard')
+  end
+
+  it "create_application_dashboard" do
+    User.any_instance.stub(:admin?).and_return(false)
+    user = User.new
+    user.stub_chain(:organization, :submitted?).and_return(false)
+    user.stub_chain(:organization, :approved?).and_return(false)
+    expect(dashboard_for(user)).to eq('create_application_dashboard')
+  end
+
 end
