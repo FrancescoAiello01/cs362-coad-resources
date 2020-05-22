83b5a0cb3bb6a74d4dc127e91c8ce75b9aa7b21c
Francesco Aiello <aiellof@oregonstate.edu>
2020-05-22 10:10:39 -0700

    add unhappy path for approve organization spec

--- spec/features/organizations/approve_organization_spec.rb
+++ spec/features/organizations/approve_organization_spec.rb
@@ -13,10 +13,15 @@ RSpec.describe 'Approving an organization', type: :feature do
 
     it "navigates to pending organizations and approves" do
       organization = create(:organization, :set_submitted)
-      visit organizations_path(organization)
-      click_on organization.name
+      visit organization_path(organization)
       click_on "Approve"
       expect(page).to have_text('has been approved.')
+    end
+
+    it "cannot approve a non-pending organization" do
+      organization = create(:organization)
+      visit organization_path(organization)
+      expect(page).not_to have_link('Approve')
     end
 
   end
