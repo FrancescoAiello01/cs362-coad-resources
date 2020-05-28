850e9292cdaa94c07af6ba64cf1c56062e496c41
Francesco Aiello <aiellof@oregonstate.edu>
2020-05-28 09:23:58 -0700

    add admin create region spec

--- spec/features/organizations/update_organization_spec.rb
+++ spec/features/organizations/update_organization_spec.rb
@@ -1,5 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe 'Updating an Organization', type: :feature do
-
+  it "should skip this feature" do
+    skip "feature not yet implemented"
+  end
 end
--- spec/features/regions/admin_creates_region_spec.rb
+++ spec/features/regions/admin_creates_region_spec.rb
@@ -2,4 +2,31 @@ require 'rails_helper'
 
 RSpec.describe 'Creating a Region', type: :feature do
 
+  let(:admin) { create(:user, :set_admin) }
+
+  before :each do
+    admin.confirm
+    log_in_as(admin)
+  end
+
+  context "create a region" do
+
+    it "should create a new region" do
+      visit regions_path
+      click_on "Add Region"
+      fill_in "region_name", with: "fake"
+      click_on "Add Region"
+      expect(page).to have_text("Region successfully created.")
+    end
+
+    it "cannot create a new region with no name" do
+      visit regions_path
+      click_on "Add Region"
+      fill_in "region_name", with: ""
+      click_on "Add Region"
+      expect(page).to have_text("Name can't be blank")
+    end
+
+  end
+
 end
