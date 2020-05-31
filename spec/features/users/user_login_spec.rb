8effc15a2692bd7699e0cb1c24be1e797eda44d0
Francesco Aiello <aiellof@oregonstate.edu>
2020-05-31 14:14:11 -0700

    add user login spec

--- spec/features/users/user_login_spec.rb
+++ spec/features/users/user_login_spec.rb
@@ -2,4 +2,29 @@ require 'rails_helper'
 
 RSpec.describe 'Logging in', type: :feature do
 
+  let(:user) { create(:user) }
+
+  before :each do
+    user.confirm
+  end
+
+  context 'user logs in' do
+
+    it 'should log in successfully' do
+      visit login_path
+      fill_in 'user_email', with: user.email
+      fill_in 'user_password', with: user.password
+      click_on 'Sign in'
+      expect(page).to have_text('Signed in successfully.')
+    end
+
+    it 'should not log in successfully' do
+      visit login_path
+      fill_in 'user_email', with: 'fake'
+      fill_in 'user_password', with: 'fake'
+      click_on 'Sign in'
+      expect(page).to have_text('Invalid Email or password.')
+    end
+  end
+
 end
